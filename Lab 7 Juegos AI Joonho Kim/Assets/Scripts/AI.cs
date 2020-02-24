6026c3d304490a80951669d070b1ec8a1a8df090
Joonho D. Kim <37599608+jkmolina@users.noreply.github.com>
2020-02-24 10:05:12 -0600

    final commit

---
+++ Lab 7 Juegos AI Joonho Kim/Assets/Scripts/AI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public NavMeshAgent agent;
+    public GameObject[] waypoints;
+    private int currWO = 0;
+
+    private float maxWaittime = 5.0f;
+    private float currWaittime = 0.0f;
+    void Start()
+    {
+
+      //  agent = GetComponent<NavMeshAgent>();
+        if (agent && waypoints.Length > 0)
+            agent.SetDestination(waypoints[currWO].transform.position);
+        //waypoints = GameObject.FindGameObjectsWithTag("Respawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (agent)
+        {
+            if (agent.remainingDistance < 0.5f)
+            {
+                currWaittime += Time.deltaTime;
+                if (currWaittime >= maxWaittime)
+                {
+                    //esto aumenta el tiempo de espera 
+                    currWaittime = 0.0f;
+                    //currWO++;
+                    //currWO %= waypoints.Length;
+
+                    //esto es para que no que los waypoints sean random 
+                    int temp = currWO;
+                    do
+                    {
+                        currWO = Random.Range(0, waypoints.Length);
+
+                    }
+                    while (currWO == temp);
+
+
+                    //para sumarle uno y darle el valor 
+                    agent.SetDestination(waypoints[currWO].transform.position);
+                }
+
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("carl"))
+        {
+            Debug.Log("you died");
+            Destroy(other.gameObject);
+        }
+
+    }
+}
---
+++ Lab 7 Juegos AI Joonho Kim/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent carl;
+    public GameObject randy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)) 
+            {
+                carl.SetDestination(hit.point);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Win"))
+        {
+            Debug.Log("you won");
+            Destroy(randy);
+            winScreen();
+            
+        }
+
+    }
+
+    public void winScreen()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+}
